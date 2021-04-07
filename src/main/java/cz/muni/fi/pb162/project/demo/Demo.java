1f1e642c90a960c60a729b79fb1f7a2a8a1866e0
xsliva <xsliva@fi.muni.cz>
2021-04-07 13:18:46 +0200

    Iteration 05 Done

--- src/main/java/cz/muni/fi/pb162/project/demo/Demo.java
+++ src/main/java/cz/muni/fi/pb162/project/demo/Demo.java
@@ -1,7 +1,7 @@
 package cz.muni.fi.pb162.project.demo;
 
 
-import cz.muni.fi.pb162.project.geometry.Square;
+import cz.muni.fi.pb162.project.geometry.RegularOctagon;
 import cz.muni.fi.pb162.project.geometry.Vertex2D;
 
 /**
@@ -17,7 +17,7 @@ public class Demo {
      * @param args command line arguments, will be ignored
      */
     public static void main(String[] args) {
-        Square square = new Square(new Vertex2D(0,0), 100);
-        System.out.println(square);
+        RegularOctagon octagon = new RegularOctagon(new Vertex2D(0,0), 1);
+        System.out.println(octagon);
     }
 }
---
+++ src/main/java/cz/muni/fi/pb162/project/geometry/Colored.java
@@ -0,0 +1,22 @@
+package cz.muni.fi.pb162.project.geometry;
+
+/**
+ * Interface for Colored objects.
+ *
+ * @author Adam Slíva
+ */
+public interface Colored {
+    /**
+     * Getter for Color.
+     *
+     * @return Color Returns Color that is set.
+     */
+    Color getColor();
+
+    /**
+     * Setter for Color.
+     *
+     * @param color Gives color that is set.
+     */
+    void setColor(Color color);
+}
